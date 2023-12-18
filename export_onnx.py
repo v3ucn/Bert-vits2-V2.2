d9b3544f03e2ea2f3f5d2a40c76c031a050f0ccc
zcxey2911 <zcxey2911@gmail.com>
2023-12-18 12:55:17 +0800

    first commit

---
+++ export_onnx.py
@@ -0,0 +1,12 @@
+from onnx_modules import export_onnx
+import os
+
+if __name__ == "__main__":
+    export_path = "BertVits2.2PT"
+    model_path = "model\\G_0.pth"
+    config_path = "model\\config.json"
+    if not os.path.exists("onnx"):
+        os.makedirs("onnx")
+    if not os.path.exists(f"onnx/{export_path}"):
+        os.makedirs(f"onnx/{export_path}")
+    export_onnx(export_path, model_path, config_path)
